f7c2aec890d32e9febcca48e31d541a243557ad8
Pawel Piotrowski <pawel.piotrowski@home.eu>
2025-07-11 15:36:55 +0200

    added vector data layer to map

--- app/map/map.tsx
+++ app/map/map.tsx
@@ -1,31 +1,57 @@
 "use client";
 
 import * as React from "react";
-import Map from "react-map-gl/mapbox-legacy";
+import Map, { Source, Layer, LayerProps } from "react-map-gl/mapbox-legacy";
 import "mapbox-gl/dist/mapbox-gl.css";
-import DeckGL from "@deck.gl/react";
 
-import {
-  lightingEffect,
-  // material,
-  INITIAL_VIEW_STATE,
-  // colorRange,
-} from "./mapconfig";
+import { INITIAL_VIEW_STATE } from "./mapconfig";
 
 const LocationAggregatorMap = () => {
+  const [mapLoaded, setMapLoaded] = React.useState(false);
+
+  const myVectorTilesetSource = {
+    type: "vector",
+    url: "mapbox://ppiotrowski.bwg5i0qk", // ZMIENIONO URL
+  };
+
+  const onMapLoad = React.useCallback(() => {
+    setMapLoaded(true);
+  }, []);
+
+  const myTilesetLayer: LayerProps = {
+    id: "my-custom-data-layer",
+    type: "line",
+    "source-layer": "export-8r3k7q", // The name of the layer within your tileset
+    layout: {
+      "line-join": "round",
+      "line-cap": "round",
+    },
+    paint: {
+      "line-color": "#ff69b4",
+      "line-width": 3,
+    },
+  };
+
   return (
     <div>
-      <DeckGL
-        effects={[lightingEffect]}
+      <Map
+        id="my-custom-tileset-source"
         initialViewState={INITIAL_VIEW_STATE}
-        controller={true}
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+        style={{ position: "relative", width: "100%", height: "100vh" }}
+        mapStyle="mapbox://styles/ppiotrowski/cmbesfnjn004t01r02wpgahpe"
+        onLoad={onMapLoad}
       >
-        <Map
-          mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-          style={{ position: "relative", width: "100%", height: "100vh" }}
-          mapStyle="mapbox://styles/mapbox/dark-v11"
-        />
-      </DeckGL>
+        {mapLoaded && (
+          <Source
+            id="custom-tileset-data-source"
+            type="vector"
+            url={myVectorTilesetSource.url}
+          >
+            <Layer {...myTilesetLayer} source="custom-tileset-data-source" />
+          </Source>
+        )}
+      </Map>
     </div>
   );
 };
